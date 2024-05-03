8c60aa68892f3f76d299f41f89723d02fadd6873
Enelg52 <yann.grossenbacher@bluewin.ch>
2024-05-03 11:17:00 +0200

    improve tests

--- test/scope_test.go
+++ test/scope_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"fmt"
 	"gotest.tools/v3/assert"
-	"log"
 	"os"
 	"reconr/internal"
 	"testing"
@@ -17,12 +16,12 @@ func init() {
 	if _, err := os.Stat(fmt.Sprintf("./log/%s.log", url)); err == nil {
 		err := os.Remove(fmt.Sprintf("./log/%s.log", url))
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
 		}
 	}
 	err := os.RemoveAll(fmt.Sprintf("./out/%s", url))
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 	time.Sleep(10 * time.Second)
 }
@@ -34,39 +33,46 @@ func Test_scope(T *testing.T) {
 	config, err := internal.NewConfig(configFile)
 	if err != nil {
 		fmt.Println(err)
+		T.Fail()
 	}
 	logger, err := internal.NewLogger(config.GetLogPath(), config.GetTarget())
 	if err != nil {
 		fmt.Println(err)
+		T.Fail()
 	}
 
 	path := fmt.Sprintf("%s/%s", config.GetWorkPath(), config.GetTarget())
 	scope, err := internal.NewScope(config.GetScope(), path)
 	if err != nil {
 		fmt.Println(err)
+		T.Fail()
 	}
 	scopePath := fmt.Sprintf("%s/%s/%s", config.GetWorkPath(), config.GetTarget(), config.GetScopeFileName())
 	err = scope.WriteScope(scopePath)
 	if err != nil {
 		fmt.Println(err)
+		T.Fail()
 	}
 
 	workflow, err := internal.NewWorkflow(workflowFile, config)
 	if err != nil {
 		fmt.Println(err)
+		T.Fail()
 	}
 
 	err = internal.Process(workflow, config, logger)
 	if err != nil {
-		//This error is caused by the task to be to fast. So the logs can not benn read.
+		//This error is caused by the task to being too fast. So the logs can't be read.
 		if err.Error() != "Error response from daemon: can not get logs from container which is dead or marked for removal" {
 			fmt.Println(err.Error())
+			T.Fail()
 		}
 	}
 
 	dat, err := os.ReadFile(fmt.Sprintf("./out/%s/validScope.txt", url))
 	if err != nil {
 		fmt.Println(err)
+		T.Fail()
 	}
 	assert.Equal(T, string(dat), check)
 }
